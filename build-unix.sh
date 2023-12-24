19b61d577f4adfe5c73f5004995969d7af6e229c
Rafael Henrique Capati <rhcapati@gmail.com>
2023-12-24 13:22:52 -0300

    initial commit

---
+++ build-unix.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+OUTPUT_DIR=examples/build
+EXECUTABLE_NAME=hello_vulkan
+
+# Ensure build folder exists
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir $OUTPUT_DIR
+fi
+
+# Build and run command
+cmd=$1
+
+if [ -z "$cmd" ]; then
+    cmd=build
+fi
+
+odin build ./examples/hello_vulkan \
+    -debug \
+    -vet \
+    -strict-style \
+    -o:none \
+    -max-error-count:1 \
+    -use-separate-modules \
+    -out:$OUTPUT_DIR/$EXECUTABLE_NAME
+
+compiler_error_code=$?
+
+if [ $compiler_error_code -ne 0 ]; then
+    echo -e "\nFailed to compile!"
+    exit 1
+fi
+
+echo -e "\nCompiled successfully!\n"
+
+if [ "$cmd" == "run" ]; then
+    cd "./$OUTPUT_DIR"
+    ./$EXECUTABLE_NAME
+fi
---
+++ build-win.bat
@@ -0,0 +1,32 @@
+@echo off
+
+set "OUTPUT_DIR=examples/build"
+set "EXECUTABLE_NAME=hello_vulkan.exe"
+
+set "type=%~1"
+if "%type%"=="" set type=build
+
+if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
+
+odin build .\examples\hello_vulkan ^
+	-debug ^
+	-vet ^
+    -strict-style ^
+	-o:none ^
+	-max-error-count:1 ^
+	-use-separate-modules ^
+	-out:%OUTPUT_DIR%/%EXECUTABLE_NAME%
+
+set compiler_error_code=%errorlevel%
+
+if %compiler_error_code% neq 0 (
+	echo Failed to compile!
+	exit 1
+)
+
+echo Compiled successfully!
+
+if "%type%"=="run" (
+	cd %OUTPUT_DIR%
+	.\%EXECUTABLE_NAME%
+)
